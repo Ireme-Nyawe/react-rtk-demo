e095d1fff010d8b248fc42932f0b9b75da1cd891
Ireme-Nyawe <akimana.inono@gmail.com>
2024-07-17 19:12:50 +0200

    dispatch cake actions

--- src/features/cake/CakeView.jsx
+++ src/features/cake/CakeView.jsx
@@ -1,12 +1,14 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { ordered,restocked } from "./cakeSlice";
 
 function CakeView() {
   const numOfCakes = useSelector((state) => state.cake.numOfCakes);
+  const dispatch = useDispatch();
   return (
     <div>
       <h2>Number of cakes- {numOfCakes}</h2>
-      <button>order a cake</button>
-      <button>restock cakes</button>
+      <button onClick={()=>dispatch(ordered())}>order a cake</button>
+      <button onClick={()=>dispatch(restocked(2))}>restock cakes</button>
     </div>
   );
 }
