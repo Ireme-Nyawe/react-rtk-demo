57030fe8e1f13aae184bdf5123f0f408260d1f6a
Ireme-Nyawe <akimana.inono@gmail.com>
2024-07-17 19:10:26 +0200

    dispatch ice actions

--- src/features/ice/IceView.jsx
+++ src/features/ice/IceView.jsx
@@ -1,13 +1,14 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { ordered,restocked } from "./iceSlice";
 function IceView() {
-    const numOfIceCream = useSelector((state) => state.iceCream.numOfIceCream);
-    return (
-      <div>
-        <h2>Number of cakes- {numOfIceCream}</h2>
-        <button>order a cake</button>
-        <button>restock cakes</button>
-      </div>
-    );
-  }
-  export default IceView;
-  
+  const numOfIceCream = useSelector((state) => state.iceCream.numOfIceCream);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>Number of icecream- {numOfIceCream}</h2>
+      <button onClick={() => dispatch(ordered())}>order iceCream</button>
+      <button onClick={() => dispatch(restocked(4))}>restock  iceCream</button>
+    </div>
+  );
+}
+export default IceView;
