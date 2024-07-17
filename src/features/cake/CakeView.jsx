f9e1422eda461eb9f8cb38b5b948ff9679cbc52a
Ireme-Nyawe <akimana.inono@gmail.com>
2024-07-17 18:25:20 +0200

    add cakeview

---
+++ src/features/cake/CakeView.jsx
@@ -0,0 +1,10 @@
+function CakeView() {
+  return (
+    <div>
+      <h2>Number of cakes-</h2>
+      <button>order a cake</button>
+      <button>restock cakes</button>
+    </div>
+  );
+}
+export default CakeView;
