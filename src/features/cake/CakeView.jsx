d96fe58c133998d017b60997ae9dc8d71aa3d9e6
Ireme-Nyawe <akimana.inono@gmail.com>
2024-07-17 19:20:07 +0200

    use input for better action payload

--- src/features/cake/CakeView.jsx
+++ src/features/cake/CakeView.jsx
@@ -1,14 +1,17 @@
 import { useDispatch, useSelector } from "react-redux";
-import { ordered,restocked } from "./cakeSlice";
+import { ordered, restocked } from "./cakeSlice";
+import { useState } from "react";
 
 function CakeView() {
   const numOfCakes = useSelector((state) => state.cake.numOfCakes);
   const dispatch = useDispatch();
+  const [value, setValue] = useState(1);
   return (
     <div>
       <h2>Number of cakes- {numOfCakes}</h2>
-      <button onClick={()=>dispatch(ordered())}>order a cake</button>
-      <button onClick={()=>dispatch(restocked(2))}>restock cakes</button>
+      <button onClick={() => dispatch(ordered())}>order a cake</button>
+      <input type="number" value={value} onChange={(e) => setValue(parseInt(e.target.value))} />
+      <button onClick={() => dispatch(restocked(value))}>restock cakes</button>
     </div>
   );
 }
